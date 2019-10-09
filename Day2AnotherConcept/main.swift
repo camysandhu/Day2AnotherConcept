989ac206f29d7803fd56df37067bb088d7645feb
camysandhu <fmsharmn@gmail.com>
2019-10-09 18:47:23 -0400

    Array

--- Day2AnotherConcept/main.swift
+++ Day2AnotherConcept/main.swift
@@ -1,12 +1,50 @@
-//
-//  main.swift
-//  Day2AnotherConcept
-//
-//  Created by MacStudent on 2019-10-09.
-//  Copyright © 2019 MacStudent. All rights reserved.
-//
+var a : [Int]
+a = [Int]()
+//print(a[0]) // Error
 
-import Foundation
+//a[0] = 100
+//print(a[0])
 
-print("Hello, World!")
+//Add new array element
+
+a.append(10)
+a.append(20)
+a.append(30)
+a.append(40)
+a.append(50)
+a.append(60)
+a.append(70)
+a.append(80)
+a.append(90)
+a.append(100)
+
+for i in a
+{
+    print(i)
+}
+print("Array by <10")
+for i in 0..<10
+{
+    print(a[i])
+}
+print("Array by 9")
+for i in 0...9
+{
+    print(a[i])
+}
+
+var b = Array.init(repeating: 0, count: 5)
+ print(b[0])
+a = a + [101, 102, 103]
+a = a + b
+print(a)
+print("a \(a.count)")
+
+
+
+var 2dArray :[[Int]]()
+
+
+
+
 
